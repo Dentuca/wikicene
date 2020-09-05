8a23a258be129b43e6145d33f1af8f05435d6cac
Ricardo Costa <95.ricardo.costa@gmail.com>
2020-09-05 10:30:01 +0100

    Adds Access-Control-Allow-Origin header to every response

--- src/main/kotlin/wikicene/api/WikiceneHandler.kt
+++ src/main/kotlin/wikicene/api/WikiceneHandler.kt
@@ -19,6 +19,7 @@ class WikiceneHandler(
         )
 
         response
+            .putHeader("Access-Control-Allow-Origin", "*")
             .putHeader("content-type", "application/json")
             .end(jsonResponse.encode())
     }
